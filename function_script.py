ed612e7780c1b7b51c197a99d1fe2114e3bbe5df
Bagas Naufal <119937815+Bagasaaa@users.noreply.github.com>
2023-03-27 15:49:46 +0700

    API done, save tokenizer with pickle

---
+++ function_script.py
@@ -0,0 +1,63 @@
+import re
+
+# Function to cleaning the data
+def cleansing(text):
+    # Make sentence being lowercase
+    text = text.lower()
+
+    # Remove user, rt, \n, retweet, \t, url, xd
+    pattern_1 = r'(user|retweet|\\t|\\r|url|xd|orang|kalo)'
+    text = re.sub(pattern_1, '', text)
+
+    # Remove mention
+    pattern_2 = r'@[^\s]+'
+    text = re.sub(pattern_2, '', text)
+
+    # Remove hashtag
+    pattern_3 = r'#([^\s]+)'
+    text = re.sub(pattern_3, '', text)
+
+    # Remove general punctuation, math operation char, etc.
+    pattern_4 = r'[\,\@\*\_\-\!\:\;\?\'\.\"\)\(\{\}\<\>\+\%\$\^\#\/\`\~\|\&\|]'
+    text = re.sub(pattern_4, ' ', text)
+
+    # Remove single character
+    pattern_5 = r'\b\w{1,3}\b'
+    text = re.sub(pattern_5, '', text)
+
+    # Remove emoji
+    pattern_6 = r'\\[a-z0-9]{1,5}'
+    text = re.sub(pattern_6, '', text)
+
+    # Remove digit character
+    pattern_7 = r'\d+'
+    text = re.sub(pattern_7, '', text)
+
+    # Remove url start with http or https
+    pattern_8 = r'(https|https:)'
+    text = re.sub(pattern_8, '', text)
+
+    # Remove (\); ([); (])
+    pattern_9 = r'[\\\]\[]'
+    text = re.sub(pattern_9, '', text)
+
+    # Remove character non ASCII
+    pattern_10 = r'[^\x00-\x7f]'
+    text = re.sub(pattern_10, '', text)
+
+    # Remove character non ASCII
+    pattern_11 = r'(\\u[0-9A-Fa-f]+)'
+    text = re.sub(pattern_11, '', text)
+
+    # Remove multiple whitespace
+    pattern_12 = r'(\s+|\\n)'
+    text = re.sub(pattern_12, ' ', text)
+
+    # Remove "wkwkwk"
+    pattern_13 = r'\bwk\w+'
+    text = re.sub(pattern_13, '', text)
+    
+    # Remove whitespace at the first and end sentences
+    text = text.rstrip()
+    text = text.lstrip()
+    return text
